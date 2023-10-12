9b3b4d47c8359a97c8fdb803d565e76254ca30f7
Tomixer <tomix.klokar@gmail.com>
2023-10-12 23:03:10 +0200

    vypisanie do stranky

--- src/components/OneMovieSlider.js
+++ src/components/OneMovieSlider.js
@@ -1,13 +1,26 @@
 import "./OneMovieSlider.css"
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
-import allMovies from "./data";
+import data from "../data"
 import { useState,useEffect } from "react";
 
 const OneMovieSlider=()=>{
     const [index,setindex]=useState(0)
 
     return <section className="all-movies">
-        
+        <div className="ass-movies-content">
+            {data.map((onemovie)=>{
+                const {id,image,title,age,tags,description}=onemovie
+                return <article key={id}>
+                    <img src={image} alt="" />
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <p>{tags}</p>
+                    <p>{age}</p>
+                </article>
+            })}
+        </div>
+        <button><FaArrowAltCircleLeft /></button>
+        <button><FaArrowAltCircleRight /></button>
     </section>
 }
 export default OneMovieSlider
