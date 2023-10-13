76b557d2607dd636c01227ecd3fc9df99a574aa4
Tomixer <tomix.klokar@gmail.com>
2023-10-13 21:20:14 +0200

    auto swipe a button funkcie

--- src/components/OneMovieSlider.js
+++ src/components/OneMovieSlider.js
@@ -6,11 +6,32 @@ import { useState,useEffect } from "react";
 const OneMovieSlider=()=>{
     const [index,setindex]=useState(0)
 
+    useEffect(()=>{
+        if(index<0){
+            setindex(data.length-1)
+        }else if(index>data.length-1){
+            setindex(0)
+        }
+    },[index])
+    useEffect(()=>{
+        let setintervalid=setInterval(()=>{
+            setindex(index+1)
+        },3000)
+        return()=>clearInterval(setintervalid)
+    },[index])
+
     return <section className="all-movies">
         <div className="all-movies-content">
-            {data.map((onemovie)=>{
+            {data.map((onemovie,onemovieindex)=>{
                 const {id,image,title,age,tags,description}=onemovie
-                return <article key={id}>
+                let mainclass="next-slide"
+                if(onemovieindex===index){
+                    mainclass="active-slide"
+                }
+                if(onemovieindex===index-1||(index===0&&onemovieindex===data.length-1)){
+                    mainclass="last-slide"
+                }
+                return <article key={id} className={mainclass}>
                     <img src={image} alt="" />
                     <h2>{title}</h2>
                     <p>{description}</p>
@@ -19,8 +40,8 @@ const OneMovieSlider=()=>{
                 </article>
             })}
         </div>
-        <button><FaArrowAltCircleLeft /></button>
-        <button><FaArrowAltCircleRight /></button>
+        <button onClick={()=>setindex(index-1)}><FaArrowAltCircleLeft /></button>
+        <button onClick={()=>setindex(index+1)}><FaArrowAltCircleRight /></button>
     </section>
 }
 export default OneMovieSlider
